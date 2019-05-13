078f9dfb725463c1567ef18f4cfcd30a14d28ae0
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-05-13 12:23:31 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,9 @@
 # require libraries/modules here
+require 'nokogiri'
+require '/fixtures/kickstarter.html'
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
   # write your code here
 end
